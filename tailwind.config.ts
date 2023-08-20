abe1cd2d1acfd136c500f59aa6ca523b46498413
Sky <1179717+skyriverbend@users.noreply.github.com>
2023-08-20 13:38:05 +0200

    Add basic layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'charcoal': {
+          600: '#72718E',
+          700: '#454456',
+          800: '#2E2D39',
+        },
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
